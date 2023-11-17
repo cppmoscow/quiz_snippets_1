abe7be6b10feb93e4d8a4f48838d1c92cfd62478
Danil Sidoruk <danilsidoruknv@yandex.ru>
2023-11-17 21:49:17 +0300

    Fix snippet_1.cpp
    Make it give compiler error

--- snippets/snippet_1.cpp
+++ snippets/snippet_1.cpp
@@ -1,7 +1,5 @@
 int negate(int i) { return -i; }
 
-template <typename T> typename T::value_type negate(const T &t) {
-  typename T::value_type n = -t();
-}
+template <typename T> T negate(const T &t) { typename T::value_type n = -t(); }
 
 int main() { negate(2.0); }
