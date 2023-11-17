88e0498e890e9f8ac98425bb241ea3b70f1300b6
Danil Sidoruk <danilsidoruknv@yandex.ru>
2023-11-17 23:33:19 +0300

    Add snippet_3

---
+++ snippets/snippet_3/main.cpp
@@ -0,0 +1,15 @@
+//-----------------------------------------------------------------------------
+//
+// Source code for MIPT masters course on C++
+// Slides: https://sourceforge.net/projects/cpp-lects-rus
+// Licensed after GNU GPL v3
+//
+//-----------------------------------------------------------------------------
+//
+// global fragment instantiation issues: usage
+//
+//-----------------------------------------------------------------------------
+
+import global;
+
+int main() { output(42); }
