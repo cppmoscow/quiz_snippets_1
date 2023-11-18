dfcc4be3ed53208ec0ca2a218c4b901b809986df
Danil Sidoruk <danilsidoruknv@yandex.ru>
2023-11-18 18:52:03 +0300

    Implement gcc compile script for snippet_3

---
+++ snippets/snippet_3/runme-gcc.sh
@@ -0,0 +1,2 @@
+g++ -std=c++20 -fmodules-ts -x c++-system-header iostream
+g++ -std=c++20 -fmodules-ts global.cpp main.cpp -o snippet_3_gcc
--- snippets/snippet_3/runme.sh
+++
@@ -1,2 +0,0 @@
-clang++ --std=c++20 global.cppm --precompile -o global.pcm
-clang++ --std=c++20 main.cpp -fprebuilt-module-path=. global.pcm -o snippet_3
